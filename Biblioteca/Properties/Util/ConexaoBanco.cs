d97ed59c0c37c4c2c4ff22a202f254d0aba6228c
leandrooliveiraads <leandro.oliveira.ads@outlook.com>
2016-11-04 20:43:57 -0300

    leandro

---
+++ Biblioteca/Properties/Util/ConexaoBanco.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Biblioteca.Properties.Util
+{
+    class ConexaoBanco
+    {
+        public SqlConnection sqlConn;
+        private const string local = @"local";
+        private const string nomeDoBanco = 
+    }
+}
